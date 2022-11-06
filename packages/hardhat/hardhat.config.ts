94845020ebe541685ef7b5bcc8e6fade00fe7276
luka1104 <skyward.luke11@gmail.com>
2022-11-06 08:09:57 -0800

    feature/edit .env

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,8 +1,21 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
+/** @type import('hardhat/config').HardhatUserConfig */
 
-const config: HardhatUserConfig = {
-  solidity: '0.8.17',
-};
-
-export default config;
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, MUMBAI_URL, PRIVATE_KEY } = process.env;
+ module.exports = {
+    solidity: "0.8.9",
+    defaultNetwork: "goerli",
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`],
+      },
+      mumbai: {
+        url: MUMBAI_URL,
+        accounts: [`0x${PRIVATE_KEY}`],
+      }
+    },
+ };
--- packages/hardhat/scripts/deploy.ts
+++ packages/hardhat/scripts/deploy.ts
@@ -1,25 +1,16 @@
-import { ethers } from 'hardhat';
+const { ethers } = require("hardhat")
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const VNFT = await ethers.getContractFactory("VminaNFT")
 
-  const lockedAmount = ethers.utils.parseEther('1');
-
-  const Lock = await ethers.getContractFactory('Lock');
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`,
-  );
+  const vnft = await VNFT.deploy()
+  await vnft.deployed()
+  console.log("Contract deployed to address:", vnft.address)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
