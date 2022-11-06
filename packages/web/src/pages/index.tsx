b1c295db78faf2154d28ef8f44282db049fb97ac
Bulent Tastan <bulenttastan@gmail.com>
2022-11-06 06:39:50 -0800

    Fix prisma video issue in other places

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,14 +1,13 @@
 import { NextPage } from 'next';
 import React from 'react'
 import type { GetServerSideProps } from "next";
-import prisma from '../lib/prisma'
-import { Video } from '.prisma/client';
-import { Box, SimpleGrid } from '@chakra-ui/react'
+import prisma from '../lib/prisma';
+import { Box, SimpleGrid } from '@chakra-ui/react';
 import Card from 'components/Card';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 type Props = {
-  videos: Video[];
+  videos: any[];
 };
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const videosRaw = await prisma.video.findMany();
@@ -20,7 +19,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
   };
 };
 interface PropTypes {
-  videos: Video[];
+  videos: any[];
 }
 
 const Index: NextPage<PropTypes> = ({ videos }) => {
@@ -31,12 +30,17 @@ const Index: NextPage<PropTypes> = ({ videos }) => {
   return (
     <>
       <SimpleGrid columns={3} spacing={20} w="100%" p="0 12%" mt="40px">
-        {videos.map((val: Video, key: number) => {
+        {videos.map((val: any, key: number) => {
           return (
-            <Box key={key} onClick={() => {router.push(`/video?id=${val.id}`)}}>
+            <Box
+              key={key}
+              onClick={() => {
+                router.push(`/video?id=${val.id}`);
+              }}
+            >
               <Card video={val} key={key} />
             </Box>
-          )
+          );
         })}
       </SimpleGrid>
     </>
