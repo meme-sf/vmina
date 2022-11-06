75e1ed8c0635e18f336ff2ec97f0456464893a0d
Bulent Tastan <bulenttastan@gmail.com>
2022-11-06 07:53:04 -0800

    Add website title

--- packages/web/src/components/Header.tsx
+++ packages/web/src/components/Header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+
 import { Box, Center, Button, Icon, Image } from '@chakra-ui/react';
+import Head from 'next/head';
 import NextLink from 'next/link';
 import { AiOutlinePlus } from 'react-icons/ai';
+import { BiWallet } from 'react-icons/bi';
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
+
 import { shortenEOAName } from 'utils/shortenEOA';
-import { BiWallet } from 'react-icons/bi'
 
 const Header: React.FC = () => {
   const { address, isConnected } = useAccount();
@@ -16,6 +19,10 @@ const Header: React.FC = () => {
   const shortAddress = shortenEOAName(address);
   return (
     <>
+      <Head>
+        <title>Vmina | Secure Video Marketplace</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Box w="100%" h="60px" p="0 30px 0 0" bg="#FFFDF6">
         <Center h="100%" justifyContent="space-between">
           <NextLink href="/" passHref>
@@ -28,11 +35,19 @@ const Header: React.FC = () => {
               </Button>
             </NextLink>
             {isConnected ? (
-              <Button leftIcon={<BiWallet />} colorScheme="orange" onClick={() => disconnect()}>
+              <Button
+                leftIcon={<BiWallet />}
+                colorScheme="orange"
+                onClick={() => disconnect()}
+              >
                 {shortAddress}
               </Button>
             ) : (
-              <Button leftIcon={<BiWallet />} colorScheme="orange" onClick={() => connect()}>
+              <Button
+                leftIcon={<BiWallet />}
+                colorScheme="orange"
+                onClick={() => connect()}
+              >
                 Connect Wallet
               </Button>
             )}
